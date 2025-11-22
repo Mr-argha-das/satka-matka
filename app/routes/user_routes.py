e7a58a9d52f0c752593a17a6b4a51bbc845e1e14
u2854668 <u2854668@uel.ac.uk>
2025-11-22 09:12:36 +0530

    changes

--- app/routes/user_routes.py
+++ app/routes/user_routes.py
@@ -176,6 +176,10 @@ def get_profile(user=Depends(get_current_user)):
         "role": user.role
     }
 
+@router.get('/me')
+def read_users_me(current_user: User = Depends(get_current_user)):
+    return json.loads(current_user.to_json())
+
 @router.get("/profile2")
 def get_profile2(user=Depends(get_current_user)):
     return {
