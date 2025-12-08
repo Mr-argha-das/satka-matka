3d1f116ddc6e8767ad75155084a3eb6f9dfaff3b
u2854668 <u2854668@uel.ac.uk>
2025-12-09 02:39:01 +0530

    changs

--- app/routes/v1/v1_autoPay_routes.py
+++ app/routes/v1/v1_autoPay_routes.py
@@ -16,8 +16,7 @@ class CreatePaymentRequest(BaseModel):
 
 
 class SMSWebhookRequest(BaseModel):
-    sms: str
-    ref_no: str
+    userId: str
     amount: float
 
 def generate_txn_id():
@@ -42,7 +41,7 @@ def create_payment(req: CreatePaymentRequest):
         status="pending"
     ).save()
 
-    upi_link = f"upi://pay?pa=hdml61i74205@hdfcbank&pn=Abhay Prakash Koli&am={req.amount}&cu=INR&tn=Paying to Kalyan Ratan 777&tr={txn_id}"
+    upi_link = f"upi://pay?pa=2977654a@bandhan&pn=Abhay Prakash Koli&am={req.amount}&cu=INR&tn=Paying to Kalyan Ratan 777&tr={txn_id}"
     
 
     return {
@@ -59,13 +58,13 @@ def sms_webhook(req: SMSWebhookRequest):
     utr = req.ref_no
 
     # Find pending transaction with same amount (best guess match)
-    txn = Transaction.objects(amount=req.amount, status="pending").first()
+    txn =  Transaction.objects(status="pending", user_id=str(req.userId)).order_by("-created_at").first()
 
     if not txn:
         return {"error": "Transaction not found"}
 
     # Mark transaction success
-    txn.status = "success"
+    txn.status = "SUCCESS"
     txn.utr = utr
     txn.save()
 
@@ -83,6 +82,34 @@ def sms_webhook(req: SMSWebhookRequest):
         "new_balance": wallet.balance
     }
 
+@router.post("/payment/sms-webhook")
+def sms_webhook(req: SMSWebhookRequest):
+    print("Received SMS webhook:", req.to_dict())
+  
+
+    # Find pending transaction with same amount (best guess match)
+    txn = Transaction.objects(amount=req.amount, status="pending", user_id=str(req.userId)).first()
+
+    if not txn:
+        return {"error": "Transaction not found"}
+
+    # Mark transaction success
+    txn.status = "SUCCESS"
+
+    txn.save()
+
+    # Add money to wallet
+    wallet = get_or_create_wallet(txn.user_id)
+    wallet.balance += txn.amount
+    wallet.updated_at = datetime.utcnow()
+    wallet.save()
+
+    return {
+        "status": "success",
+        "message": "Wallet credited",
+        "txn_id": txn.txn_id,
+        "new_balance": wallet.balance
+    }
 @router.get("/wallet/{user_id}")
 def get_wallet(user_id: str):
     wallet = get_or_create_wallet(user_id)
